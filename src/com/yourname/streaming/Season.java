4e646c40f81ab4c7ff494fd41127a248ba712ff5
SatimAskhat <s23067494@narxoz.kz>
2025-04-26 11:12:44 +0500

    Update Season.java

--- src/com/yourname/streaming/Season.java
+++ src/com/yourname/streaming/Season.java
@@ -1,1 +1,23 @@
+package com.yourname.streaming;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Season implements Iterable<Episode> {
+    private List<Episode> episodes = new ArrayList<>();
+
+    public void addEpisode(Episode e) {
+        episodes.add(e);
+    }
+
+    public List<Episode> getEpisodes() {
+        return episodes;
+    }
+
+    @Override
+    public Iterator<Episode> iterator() {
+        return new SeasonIterator(this);
+    }
+}
 
