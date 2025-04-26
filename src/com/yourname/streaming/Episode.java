a71ea6a30b79a0fe809cb5b8ff3a6adea66ba691
SatimAskhat <s23067494@narxoz.kz>
2025-04-26 11:11:36 +0500

    Update Episode.java

--- src/com/yourname/streaming/Episode.java
+++ src/com/yourname/streaming/Episode.java
@@ -1,1 +1,24 @@
+package com.yourname.streaming;
 
+public class Episode {
+    private String title;
+    private int runtimeSec;
+
+    public Episode(String title, int runtimeSec) {
+        this.title = title;
+        this.runtimeSec = runtimeSec;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getRuntimeSec() {
+        return runtimeSec;
+    }
+
+    @Override
+    public String toString() {
+        return title + " (" + runtimeSec + " sec)";
+    }
+}
