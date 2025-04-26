94849239ef7189297327287cbac36755d2aa6679
SatimAskhat <s23067494@narxoz.kz>
2025-04-26 11:16:45 +0500

    Create BingeIterator.java

---
+++ src/com/yourname/streaming/BingeIterator.java
@@ -0,0 +1,35 @@
+package com.yourname.streaming;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class BingeIterator implements EpisodeIterator {
+    private List<Season> seasons;
+    private int seasonIndex = 0;
+    private EpisodeIterator currentIterator;
+
+    public BingeIterator(Series series) {
+        this.seasons = series.getSeasons();
+        if (!seasons.isEmpty()) {
+            currentIterator = new SeasonIterator(seasons.get(0));
+        }
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (currentIterator != null && !currentIterator.hasNext()) {
+            seasonIndex++;
+            if (seasonIndex < seasons.size()) {
+                currentIterator = new SeasonIterator(seasons.get(seasonIndex));
+            } else {
+                return false;
+            }
+        }
+        return currentIterator != null && currentIterator.hasNext();
+    }
+
+    @Override
+    public Episode next() {
+        return currentIterator.next();
+    }
+}
