b20b49d9c5690699b2f74d3f0f20c2d86cbd24ff
SatimAskhat <s23067494@narxoz.kz>
2025-04-26 11:16:13 +0500

    Create ShuffleSeasonIterator.java

---
+++ src/com/yourname/streaming/ShuffleSeasonIterator.java
@@ -0,0 +1,26 @@
+package com.yourname.streaming;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class ShuffleSeasonIterator implements EpisodeIterator {
+    private List<Episode> shuffled;
+    private int index = 0;
+
+    public ShuffleSeasonIterator(Season season, long seed) {
+        shuffled = new ArrayList<>(season.getEpisodes());
+        Collections.shuffle(shuffled, new Random(seed));
+    }
+
+    @Override
+    public boolean hasNext() {
+        return index < shuffled.size();
+    }
+
+    @Override
+    public Episode next() {
+        return shuffled.get(index++);
+    }
+}
